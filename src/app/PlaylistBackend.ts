df2048d810cadd459d6cf6493e85c0f1ba10dc71
Sander Mak <sander.mak@luminis.eu>
2015-10-11 21:27:43 +0200

    Show playlist

---
+++ src/app/PlaylistBackend.ts
@@ -0,0 +1,40 @@
+import { ConcertSummary } from './YTLiveBackend';
+
+export interface PlaylistService {
+    getPlaylist(): ConcertSummary[];
+    addConcert(concert: ConcertSummary): void;
+    removeConcert(id: string): void;
+}
+
+export class LocalStoragePlayList implements PlaylistService {
+
+    static PLAYLIST_KEY = 'ytlive-playlist';
+
+    addConcert(concert: ConcertSummary): void {
+        var playlist = this.getPlaylistInternal();
+        if(!playlist.some(c => c.id === concert.id)) {
+            playlist.unshift(concert);
+        }
+        localStorage.setItem(LocalStoragePlayList.PLAYLIST_KEY, JSON.stringify(playlist));
+    }
+
+    removeConcert(id: string): void {
+        var playlist = this.getPlaylistInternal();
+        playlist = playlist.filter(c => c.id !== id);
+        localStorage.setItem(LocalStoragePlayList.PLAYLIST_KEY, JSON.stringify(playlist));
+    }
+
+    getPlaylist(): ConcertSummary[] {
+        return this.getPlaylistInternal();
+    }
+
+    getPlaylistInternal(): ConcertSummary[] {
+        var playlistString = localStorage.getItem(LocalStoragePlayList.PLAYLIST_KEY);
+        var playlist: ConcertSummary[] =  playlistString ? JSON.parse(playlistString) : [];
+
+        if(!playlistString) {
+            localStorage.setItem(LocalStoragePlayList.PLAYLIST_KEY, JSON.stringify(playlist));
+        }
+        return playlist;
+    }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import {Component, Directive, View, bootstrap, NgFor, NgIf} from 'angular2/angular2';
-import * as backend from './YTLiveBackend';
+import * as ytbackend from './YTLiveBackend';
+import * as plbackend from './PlaylistBackend';
 
 @Component({
   selector: 'search-result',
@@ -12,43 +13,61 @@ import * as backend from './YTLiveBackend';
   directives: []
 })
 class SearchResultComponent {
-  concert: backend.ConcertSummary
-
-  constructor() {
-    console.log("concert", this.concert);
-  }
-
+  concert: ytbackend.ConcertSummary
 }
 
 @Component({
   selector: 'search',
-  viewBindings: [backend.ConcertService]
+  bindings: [ytbackend.ConcertService]
 })
 @View({
   templateUrl: "app/search.html",
   directives: [NgFor, NgIf, SearchResultComponent]
 })
 class SearchComponent {
-  concerts: backend.ConcertSummary[]
+  concerts: ytbackend.ConcertSummary[]
 
-  constructor(concertService: backend.ConcertService) {
-    console.log("SearchComponent instantiated")
+  constructor(concertService: ytbackend.ConcertService) {
     this.concerts = concertService.findConcerts("some artist");
   }
 }
 
+@Component({
+  selector: 'playlist-entry',
+  properties: ["entry"]
+})
+@View({
+  templateUrl: "app/playlistentry.html",
+  directives: []
+})
+class PLaylistEntryComponent {
+  entry: ytbackend.ConcertSummary
+}
+
+@Component({
+  selector: 'playlist',
+  bindings: [plbackend.LocalStoragePlayList]
+})
+@View({
+  templateUrl: "app/playlist.html",
+  directives: [NgFor, PLaylistEntryComponent]
+})
+class PlaylistComponent {
+  entries: ytbackend.ConcertSummary[]
+  constructor(playlistService: plbackend.LocalStoragePlayList) {
+    this.entries = playlistService.getPlaylist();
+  }
+}
 
 @Component({
   selector: 'yt-live'
 })
 @View({
   templateUrl: "app/ytlive.html",
-  directives: [SearchComponent]
+  directives: [SearchComponent, PlaylistComponent]
 })
 class YTLiveComponent {
 
 }
-
-
 
 bootstrap(YTLiveComponent);
